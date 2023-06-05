e414ae6faf953fcbd4f4b8b6fd6b31cee3495bd5
Alper Sancılı <alpersancili0@gmail.com>
2023-06-05 11:31:59 +0300

    turtledrawing added

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ drawing_board.title("Python Turtle")
 
 turtle_instance = turtle.Turtle()
 num_sides = 6
-angle = 360.0 / num_sides
+angle = 360.0 / num_sidesclgit 
 size_lenght = 100
 
 for i in range(num_sides):
